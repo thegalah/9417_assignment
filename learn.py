67ce21f3947819d52f6c7a8bb030d34072531cb2
Matthew Duong <matt@timeweave.com.au>
2015-06-03 06:30:22 +1000

    Fixed file opening

--- learn.py
+++ learn.py
@@ -1,4 +1,5 @@
 import os
+import codecs
 
 #load vocabulary
 with open ("vocabulary.txt", "r") as myfile:
@@ -6,16 +7,38 @@ with open ("vocabulary.txt", "r") as myfile:
 
 
 #path to training set
-training_set='../20news-bydate/20news-bydate-test/'
+training_set='../20news-bydate/20news-bydate-train/'
 
 #get list of classes
 V=os.listdir(training_set)
 
 #fix for macos
-V.remove('.DS_Store')
+try:
+	V.remove('.DS_Store')
+except ValueError:
+	pass
 
+#count number of training documents
+count=0
+for v in V:
+	count=count+len(os.listdir(training_set+v))
+
+print(count)
 #foreach class V
 for v in V:
 	#get documents
 	docs=os.listdir(training_set+v)
-	print(len(docs))
+	#fix for macos
+	try:
+		docs.remove('.DS_Store')
+	except ValueError:
+		pass
+	#P(v_j)
+	probability_vj=len(docs)/count
+	text_j='';
+	for doc in docs:
+		doc_path=training_set+v+'/'+doc
+		print (doc_path)
+		myfile=open(doc_path,'r')
+		file_string=myfile.read()
+		text_j=text_j+' '+file_string
