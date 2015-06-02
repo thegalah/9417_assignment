878b0714a50ea0db8fe71b7551be23092fe16ddb
Matthew Duong <matt@timeweave.com.au>
2015-06-03 00:05:25 +1000

    Added retreival of vocab

--- learn.py
+++ learn.py
@@ -1,9 +1,14 @@
 import os
 
+#load vocabulary
+with open ("vocabulary.txt", "r") as myfile:
+    vocab=myfile.read().strip().split('\n')
+
+
 #path to training set
 training_set='../20news-bydate/20news-bydate-test/'
 
 #get list of classes
 V=os.listdir(training_set)
 
-print(V)
+#foreach class V
